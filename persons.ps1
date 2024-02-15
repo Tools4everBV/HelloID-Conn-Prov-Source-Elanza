3867371bae2fce1d6494140aea4534ea9491ccdf
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-02-15 08:53:52 +0100

    Create persons.ps1

---
+++ persons.ps1
@@ -0,0 +1,132 @@
+##################################################
+# HelloID-Conn-Prov-Source-Elanza-Persons
+#
+# Version: 1.0.0
+##################################################
+# Initialize default value's
+$config = $configuration | ConvertFrom-Json
+
+# Set debug logging
+switch ($($config.IsDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+# Keep track of retrieved products
+$retrievedProducts = @{}
+
+#region functions
+function Get-ElanzaProductById {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        $Id
+    )
+
+    if (-not $retrievedProducts.ContainsKey($Id)) {
+        $productDetails = Invoke-ElanzaRestMethod -Uri "product/$Id"
+        $retrievedProducts[$Id] = $productDetails
+    }
+    Write-Output $retrievedProducts[$Id]
+}
+
+function Invoke-ElanzaRestMethod {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Uri
+    )
+    process {
+        try {
+            $headers = [System.Collections.Generic.Dictionary[[String], [String]]]::new()
+            $headers.Add("elanza-api-key-v1", "$($config.ApiKey)")
+
+            $splatParams = @{
+                Uri         = "$($config.BaseUrl)/rest-api/v1/$Uri"
+                Method      = 'GET'
+                ContentType = 'application/json'
+                Headers     =  $headers
+            }
+            Invoke-RestMethod @splatParams -Verbose:$false
+        } catch {
+            if (($Uri -like "department/*" -or $Uri -like "product/*") -and $_.Exception.Response.StatusCode -eq 'NotFound') {
+                $id = ($Uri -split "/")[-1]
+                Write-Warning "A product or department with id: [$id] cannot be found!"
+            } else {
+                $PSCmdlet.ThrowTerminatingError($_)
+            }
+        }
+    }
+}
+
+function Resolve-ElanzaError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+
+        try {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+            $errorMessage = (($ErrorObject.ErrorDetails.Message | ConvertFrom-Json)).message
+            $httpErrorObj.FriendlyMessage = $errorMessage
+        } catch {
+            $httpErrorObj.FriendlyMessage = "Received an unexpected response. The JSON could not be converted, error: [$($_.Exception.Message)]. Original error from web service: [$($ErrorObject.Exception.Message)]"
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+try {
+    $past = (Get-Date).AddDays(-$($config.PastDays)).ToString("yyyy-MM-ddTHH:mm:ssZ")
+    $future = (Get-Date).AddDays($($config.FutureDays)).ToString("yyyy-MM-ddTHH:mm:ssZ")
+    $response = Invoke-ElanzaRestMethod -Uri "plannedWorkers?from=$past&to=$future"
+    foreach ($worker in $response.workers) {
+
+        $contracts = [System.Collections.Generic.List[object]]::new()
+        foreach ($shift in $worker.shifts){
+            if (-not[string]::IsNullOrEmpty($shift.productUuid)){
+                $productDetails = Get-ElanzaProductById -Id $shift.productUuid
+            }
+
+            $shift | Add-Member -MemberType 'NoteProperty' -Name 'ExternalId' -Value $shift.uuid
+            $shift | Add-Member -MemberType 'NoteProperty' -Name 'Product'    -Value $productDetails
+            $contracts.Add($shift)
+        }
+
+        $personObj = [PSCustomObject]@{
+            ExternalId  = $worker.worker.workerNumber
+            DisplayName = "$($worker.worker.firstName) $($worker.worker.lastName)".Trim(' ')
+            FirstName   = $worker.worker.firstName
+            LastName    = $worker.worker.lastName
+            Email       = $worker.worker.email
+            PhoneNumber = $worker.worker.phoneNumber
+            Contracts   = $contracts
+        }
+
+        Write-Output $personObj | ConvertTo-Json -Depth 20
+    }
+} catch {
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-ElanzaError -ErrorObject $ex
+        Write-Verbose "Could not import Elanza persons. Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+        Write-Error "Could not import Elanza persons. Error: $($errorObj.FriendlyMessage)"
+    } else {
+        Write-Verbose "Could not import Elanza persons. Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+        Write-Error "Could not import Elanza persons. Error: $($errorObj.FriendlyMessage)"
+    }
+}
+
