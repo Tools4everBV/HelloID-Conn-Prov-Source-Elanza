57b3d433d0325c19dd4dd6b28b57bf3b71b544b7
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2025-05-30 09:41:39 +0200

    Updates after first implementation

--- persons.ps1
+++ persons.ps1
@@ -1,9 +1,9 @@
 ##################################################
 # HelloID-Conn-Prov-Source-Elanza-Persons
 #
-# Version: 1.0.0
+# Version: 1.1.0
 ##################################################
-# Initialize default value's
+# Initialize default values
 $config = $configuration | ConvertFrom-Json
 
 # Set debug logging
@@ -47,14 +47,16 @@ function Invoke-ElanzaRestMethod {
                 Uri         = "$($config.BaseUrl)/rest-api/v1/$Uri"
                 Method      = 'GET'
                 ContentType = 'application/json'
-                Headers     =  $headers
+                Headers     = $headers
             }
             Invoke-RestMethod @splatParams -Verbose:$false
-        } catch {
+        }
+        catch {
             if (($Uri -like "department/*" -or $Uri -like "product/*") -and $_.Exception.Response.StatusCode -eq 'NotFound') {
                 $id = ($Uri -split "/")[-1]
                 Write-Warning "A product or department with id: [$id] cannot be found!"
-            } else {
+            }
+            else {
                 $PSCmdlet.ThrowTerminatingError($_)
             }
         }
@@ -80,7 +82,8 @@ function Resolve-ElanzaError {
             $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
             $errorMessage = (($ErrorObject.ErrorDetails.Message | ConvertFrom-Json)).message
             $httpErrorObj.FriendlyMessage = $errorMessage
-        } catch {
+        }
+        catch {
             $httpErrorObj.FriendlyMessage = "Received an unexpected response. The JSON could not be converted, error: [$($_.Exception.Message)]. Original error from web service: [$($ErrorObject.Exception.Message)]"
         }
         Write-Output $httpErrorObj
@@ -88,13 +91,33 @@ function Resolve-ElanzaError {
 }
 #endregion
 
-try {
-    $historicalDays = (Get-Date).ToUniversalTime().AddDays(-$($config.HistoricalDays))
-    $futureDays = (Get-Date).ToUniversalTime().AddDays($($config.FutureDays))
-    $response = Invoke-ElanzaRestMethod -Uri "plannedWorkers?from=$($historicalDays.ToString('yyyy-MM-ddTHH:mm:ssZ'))&to=$($futureDays.ToString('yyyy-MM-ddTHH:mm:ssZ'))"
-    foreach ($worker in $response.workers) {
 
-        $detailsStartDate = (Get-Date).ToUniversalTime().AddDays(-$($config.ShiftDetailsStart)).Date
+try {
+    # Get all departments
+    $departments = (Invoke-ElanzaRestMethod -Uri "departments").departments
+
+    # Sort departments on uuid (to make sure the order is always the same)
+    $departments = $departments | Sort-Object -Property uuid
+
+    # Group on uuid (to match to shifts)
+    $departmentsGrouped = $departments | Group-Object uuid -AsString -AsHashTable
+
+    # Get all skills
+    $skills = (Invoke-ElanzaRestMethod -Uri "skills").skills
+
+    # Sort skills on uuid (to make sure the order is always the same)
+    $skills = $skills | Sort-Object -Property uuid
+
+    # Group on uuid (to match to shifts)
+    $skillsGrouped = $skills | Group-Object uuid -AsString -AsHashTable
+
+    # Get all workers with their shifts
+    $historicalDays = (Get-Date).ToUniversalTime().AddDays( - $($config.HistoricalDays))
+    $futureDays = (Get-Date).ToUniversalTime().AddDays($($config.FutureDays))
+    $workers = (Invoke-ElanzaRestMethod -Uri "plannedWorkers?from=$($historicalDays.ToString('yyyy-MM-ddTHH:mm:ssZ'))&to=$($futureDays.ToString('yyyy-MM-ddTHH:mm:ssZ'))").workers
+
+    foreach ($worker in $workers) {
+        $detailsStartDate = (Get-Date).ToUniversalTime().AddDays( - $($config.ShiftDetailsStart)).Date
         $detailsEndDate = $futureDays
 
         #$historyStartDate = $historicalDays
@@ -108,38 +131,86 @@ try {
 
         # Create the object that will hold all historical data as one aggregated object with a custom type
         $historicalShiftContract = @{
-            ExternalId  = $($worker.worker.workerNumber)
-            Shifts      = [System.Collections.Generic.List[object]]::new()
-            Type        = 'HistoricalShift'
-            ProductName = 'unavailable'
+            ExternalId            = $($worker.worker.workerNumber)
+            Shifts                = [System.Collections.Generic.List[object]]::new()
+            Type                  = 'HistoricalShift'
 
             # Add the same fields as for shift. Otherwise, the HelloID mapping will fail
             # The value of both the 'startAt' and 'endAt' cannot be null. If empty, HelloID is unable
             # to determine the start/end date, resulting in the contract marked as 'active'.
-            startAt        = $historyStartDate.ToString('yyyy-MM-ddTHH:mm:ssZ')
-            endAt          = $historyEndDate.ToString('yyyy-MM-ddTHH:mm:ssZ')
-            title          = 'unavailable'
-            productUuid    = 'unavailable'
-            departmentUuid = 'unavailable'
+            # startAt               = $historyStartDate.ToString('yyyy-MM-ddTHH:mm:ssZ')
+            # endAt                 = $historyEndDate.ToString('yyyy-MM-ddTHH:mm:ssZ')
+            startAt               = $historyStartDate.ToString('yyyy-MM-dd')
+            endAt                 = $historyEndDate.ToString('yyyy-MM-dd')
+            title                 = 'unavailable'
+            productUuid           = 'unavailable'
+            departmentUuid        = 'unavailable'
+            ProductName           = 'unavailable'
+            DepartmentName        = 'unavailable'
+            DepartmentCode        = 'unavailable'
+            SkillName             = 'unavailable'
+            DepartmentBrandName   = 'unavailable'
+            ProductHrFunctionCode = 'unavailable'
         }
         $contracts.Add($historicalShiftContract)
 
-        foreach ($shift in $worker.shifts){
-            if (![string]::IsNullOrEmpty($shift.StartAt)){
-                $shiftStart = [DateTime]::Parse($shift.startAt)
+        foreach ($shift in $worker.shifts) {
+            if (![string]::IsNullOrEmpty($shift.StartAt)) {
+                # $shiftStart = [DateTime]::Parse($shift.startAt)
+                $shiftStart = [DateTime]::ParseExact($shift.startAt, "MM/dd/yyyy HH:mm:ss", [System.Globalization.CultureInfo]::InvariantCulture)
 
                 if ($shiftStart -ge $detailsStartDate -and $shiftStart -le $detailsEndDate) {
-                    if (-not[string]::IsNullOrEmpty($shift.productUuid)){
-                        $productDetails = Get-ElanzaProductById -Id $shift.productUuid
-                    }
                     $shift | Add-Member -MemberType 'NoteProperty' -Name 'ExternalId'  -Value $shift.uuid
-                    $shift | Add-Member -MemberType 'NoteProperty' -Name 'ProductName' -Value $productDetails.name
                     $shift | Add-Member -MemberType 'NoteProperty' -Name 'Type'        -Value 'ActiveShift'
+
+                    # Enhance shift with product for extra information, such as: name - adjust to meet your needs
+                    # As there is no 'Get all' API for products, we need to call the API for each product
+                    if (-not[string]::IsNullOrEmpty($shift.productUuid)) {
+                        $shiftProduct = Get-ElanzaProductById -Id $shift.productUuid
+                    }
+                    if ($null -ne $shiftProduct) {
+                        # In case multiple are found with the same ID, we always select the first one in the array
+                        $shiftProduct = $shiftProduct | Select-Object -First 1
+
+                        if (![string]::IsNullOrEmpty($shiftProduct)) {
+                            $shift | Add-Member -MemberType 'NoteProperty' -Name 'ProductName' -Value $shiftProduct.name
+                            $shift | Add-Member -MemberType 'NoteProperty' -Name 'ProductHrFunctionCode' -Value $shiftProduct.hrFunctionCode
+                        }
+                    }
+
+                    # Enhance shift with department for extra information, such as: name and code - adjust to meet your needs
+                    $shiftDepartment = $departmentsGrouped["$($shift.departmentUuid)"]
+                    if ($null -ne $shiftDepartment) {
+                        # In case multiple are found with the same ID, we always select the first one in the array
+                        $shiftDepartment = $shiftDepartment | Select-Object -First 1
+
+                        if (![string]::IsNullOrEmpty($shiftDepartment)) {
+                            $shift | Add-Member -MemberType 'NoteProperty' -Name 'DepartmentName' -Value $shiftDepartment.name
+                            $shift | Add-Member -MemberType 'NoteProperty' -Name 'DepartmentCode' -Value $shiftDepartment.code
+                            $shift | Add-Member -MemberType 'NoteProperty' -Name 'DepartmentBrandName' -Value $shiftDepartment.brandName
+                        }
+                    }
+
+                    # Enhance shift with skill for extra information, such as: name - adjust to meet your needs
+                    foreach ($skillUuid in $shift.skillUuids) {
+                        $shiftSkill = $skillsGrouped["$($skillUuid)"]
+                        if ($null -ne $shiftSkill) {
+                            # In case multiple are found with the same ID, we always select the first one in the array
+                            $shiftSkill = $shiftSkill | Select-Object -First 1
+                        
+                            if (![string]::IsNullOrEmpty($shiftSkill)) {
+                                $shift | Add-Member -MemberType 'NoteProperty' -Name 'SkillName' -Value $shiftSkill.name
+                            }
+                        }
+                    }
+
                     $contracts.Add($shift)
-                } elseif ($shiftStart -ge $historyStartDate -and $shiftStart -le $historyEndDate) {
+                }
+                elseif ($shiftStart -ge $historyStartDate -and $shiftStart -le $historyEndDate) {
                     $historicalShiftContract['Shifts'].Add($shift)
                 }
-            } else {
+            }
+            else {
                 Write-Verbose "Attribute: [startAt] is empty. Import failed for shift with id: [$($shift.uuid)] and worker: [$($worker.worker.workerNumber)]."
             }
         }
@@ -156,16 +227,17 @@ try {
 
         Write-Output $personObj | ConvertTo-Json -Depth 20
     }
-} catch {
+}
+catch {
     $ex = $PSItem
     if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
         $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
         $errorObj = Resolve-ElanzaError -ErrorObject $ex
         Write-Verbose "Could not import Elanza persons. Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
         Write-Error "Could not import Elanza persons. Error: $($errorObj.FriendlyMessage)"
-    } else {
+    }
+    else {
         Write-Verbose "Could not import Elanza persons. Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
-        Write-Error "Could not import Elanza persons. Error: $($errorObj.FriendlyMessage)"
+        Write-Error "Could not import Elanza persons. Error: $($ex.Exception.Message)"
     }
 }
-
